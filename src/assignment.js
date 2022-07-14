4f7da3be2904324fcb2fe11b9921832041b39b2d
Vivian Narh <99913062+VivianNarh@users.noreply.github.com>
2022-07-14 11:54:20 +0000

    check challenge 1 check challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum= 0;
+  for (let i=0; i < arrayOfNumbers.length; i++){
+      sum += arrayOfNumbers[i];
+  }
+
+    return sum;
+  //return arraysOfNumber. length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +36,22 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+      let count =0;
+  for ( let i = 0; i < arrayOfNumbers.length; i++) {
+    if (i === 0 || i % 2 !==0) {
+       count;
+      
+     }
+     else {
+       count +=1;
+     }
+    
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ return count;
+
+    //return arrayOfNumbers.length;
+}
+  assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
